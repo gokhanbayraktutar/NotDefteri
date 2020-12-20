5997a886b762175931cb417a13e38a13e249c6f4
Gokhan <bayraktutargokhan@gmail.com>
2020-12-20 22:32:06 +0300

    Note listeleme sayfası eklendi.

---
+++ NotDefteri.Web/Controllers/NoteController.cs
@@ -0,0 +1,24 @@
+﻿using NotDefteri.Abstraction.IRepository;
+using NotDefteri.Data.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace NotDefteri.Web.Controllers
+{
+    public class NoteController : Controller
+    {
+        private readonly INoteService _noteService;
+
+        public NoteController(INoteService noteService)
+        {
+            _noteService = noteService;
+        }
+
+        public ActionResult Index()
+        {
+            List<NoteModel> noteModels = _noteService.GetAll(); 
+
+            return View(noteModels);
+        }
+    }
+}
